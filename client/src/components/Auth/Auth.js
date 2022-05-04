3606351a4e0679c5aae1d41a3bfd2b5e89707f80
--unset <tommydate0@gmail.com>
2022-05-04 20:44:49 +0300

    edit auth

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+  TextField,
+} from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+import useStyles from './styles';
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const classes = useStyles();
+
+  const isSignUp = false;
+
+  const handleSubmit = () => {};
+  const handleChange = () => {};
+
+  return (
+    <Container component='main' maxWidth='xs'>
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+
+        <Typography variant='h5'>{isSignUp ? 'Sing up' : 'Sign In'}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignUp && (
+              <>
+                <TextField
+                  name='firstName'
+                  label='First Name'
+                  handleChange={handleChange}
+                ></TextField>
+              </>
+            )}
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Auth;
