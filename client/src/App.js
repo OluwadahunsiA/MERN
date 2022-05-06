fad1ea74893e248741368f6819c5f8c8ea8bc3a6
--unset <tommydate0@gmail.com>
2022-05-06 20:40:35 +0300

    search post

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Container } from '@material-ui/core';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/home';
--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -1,14 +1,26 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  mainContainer: {
+  appBarSearch: {
+    borderRadius: 4,
+    marginBottom: '1rem',
     display: 'flex',
-    alignItems: 'center',
+    padding: '16px'
+  },
+  pagination: {
+    borderRadius: 4,
+    marginTop: '1rem',
+    padding: '16px'
+  },
+  gridContainer: {
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column-reverse'
+    }
   },
   smMargin: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(1)
   },
   actionDiv: {
-    textAlign: 'center',
-  },
+    textAlign: 'center'
+  }
 }));
